b8b04b524b7a2adbcc3629d67d102eb9d7817a18
Eric Azevedo Carvalho <snuknu@gmail.com>
2026-08-19 23:36:54 -0300

    first commit

---
+++ src/main/java/snuknu/biblion/BiblionApplication.java
@@ -0,0 +1,13 @@
+package snuknu.biblion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiblionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiblionApplication.class, args);
+	}
+
+}
